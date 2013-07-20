a7ec387ffbfd7933c286613af4f29e5602c7a40f
Santiago Villa Fernandez <svillafernandez@gmail.com>
2013-07-20 15:57:39 -0300

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
   fs.readFile('./index.html', function read(err, data) {
